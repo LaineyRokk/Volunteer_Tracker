3d73c2b16a1520bdf1114bd49cb853b1f35cc3a0
lainey <laineyNrokk@gmail.com>
2017-09-27 05:44:43 -0700

    add == method and find by id method to projects

--- lib/project.rb
+++ lib/project.rb
@@ -18,3 +18,12 @@ class Project
   def save()
       result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
       @id = result.first["id"].to_i()
+
+  def ==(another_project)
+    self.id.==(another_project.id) and self.title.==(another_project.title)
+  end
+
+  def self.find(id)
+    found_project = DB.exec("SELECT * FROM projects WHERE id = #{id}").first
+    Project.new({title: found_project['title'], id: found_project['id'].to_i})
+  end
